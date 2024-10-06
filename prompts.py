5b2fde2f79346a46c35d5f3d1e552b394545382f
Justin Chang <justinchangmusic@gmail.com>
2024-10-06 01:51:20 -0400

    Multi Image Prompt: add multi image prompt

--- prompts.py
+++ prompts.py
@@ -19,3 +19,41 @@ class HumanActionPrompt(BasePrompt):
         prompt += 'Respond with a valid JSON object:\n{\n    "Description": "describe the person",\n    "Action": "chosen action",\n    "Confidence": confidence score (0 to 1)\n}'
 
         return prompt
+
+
+MULTI_IMAGE_PROMPT = """
+You will be given 3 images, each with a different level of cropping.
+The first image is not cropped at all to give full context.
+The second image is cropped to show the person to classify with a purple box and some of the background.
+The third image is cropped to show the person to classify.
+
+Classify the action of the person in the **purple box**. \
+For object interactions the object must be visible.
+
+Choose from the list:
+
+{actions}
+
+Respond with a JSON object with the following format:
+{{
+    "Description": describe the person you're classifying
+    "Action": the action the person is performing
+    "Confidence": confidence in the prediction
+}}
+"""
+
+
+class MultiImageHumanActionPrompt(BasePrompt):
+    actions: list[str]
+
+    def __init__(self, actions: list[str]):
+        self.actions = actions
+
+    def prompt(self) -> str:
+        return (
+            MULTI_IMAGE_PROMPT.format(
+                actions="\n".join(f"- {action}" for action in self.actions)
+            )
+            .strip()
+            .strip("\n")
+        )

7a0fdc021dd0746bf536dbd62ec45e8961b0b5f5
Justin Chang <justinchangmusic@gmail.com>
2025-02-17 14:30:52 -0500

    Add chat gpt model

---
+++ v2/models/chat_gpt.py
@@ -0,0 +1,56 @@
+import base64
+import io
+from time import sleep
+
+from openai import OpenAI
+from PIL import Image
+
+from .base import BaseVisionLanguageModel
+
+
+class Gpt4oMini(BaseVisionLanguageModel):
+    client: OpenAI
+    max_tokens: int
+    sleep_time: int
+
+    def __init__(self, max_tokens: int = 1024, sleep_time: int = 5) -> None:
+        self.client = OpenAI()
+        self.max_tokens = max_tokens
+        self.sleep_time = sleep_time
+
+    def predict(self, images: list[Image.Image], prompt: str) -> str:
+        images = [
+            image.convert("RGB") if image.mode != "RGB" else image for image in images
+        ]
+
+        img_byte_arrs = []
+        for image in images:
+            img_byte_arr = io.BytesIO()
+            image.save(img_byte_arr, format="JPEG")
+            img_byte_arrs.append(img_byte_arr.getvalue())
+
+        response = self.client.chat.completions.create(
+            model="gpt-4o-mini",
+            max_tokens=self.max_tokens,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": prompt},
+                        *[
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/jpeg;base64,{base64.b64encode(img_byte_arr).decode('utf-8')}",
+                                    "detail": "low",
+                                },
+                            }
+                            for img_byte_arr in img_byte_arrs
+                        ],
+                    ],
+                },
+            ],
+        )
+
+        sleep(self.sleep_time)
+        return response.choices[0].message.content.replace("action: ", "").strip()

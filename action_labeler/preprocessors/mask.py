60c938f490a1bcaf98c0dad3b7eef40d8e1b6ea7
Justin Chang <justinchangmusic@gmail.com>
2025-07-05 11:31:52 -0400

    Remove background mask preprocessor

--- action_labeler/preprocessors/mask.py
+++ action_labeler/preprocessors/mask.py
@@ -1,14 +1,14 @@
 from PIL.Image import Image
 
 from action_labeler.detections.detection import Detection
-from action_labeler.helpers.image_helpers import add_background_mask, add_mask
+from action_labeler.helpers.image_helpers import add_mask
 from action_labeler.preprocessors.base import IPreprocessor
 
 
 class MaskPreprocessor(IPreprocessor):
     """Apply mask for current detection."""
 
-    def __init__(self, opacity: float = 0.5, color: tuple[int, int, int] = (255, 0, 0)):
+    def __init__(self, opacity: float = 0.5, color: str = "red"):
         self.opacity = opacity
         self.color = color
 
@@ -19,23 +19,5 @@ class MaskPreprocessor(IPreprocessor):
         detections: Detection,
     ) -> Image:
         return add_mask(
-            image, index, detections, opacity=self.opacity, color=self.color
-        )
-
-
-class BackgroundMaskPreprocessor(IPreprocessor):
-    """Apply mask for current detection."""
-
-    def __init__(self, opacity: float = 0.5, color: tuple[int, int, int] = (255, 0, 0)):
-        self.opacity = opacity
-        self.color = color
-
-    def preprocess(
-        self,
-        image: Image,
-        index: int,
-        detections: Detection,
-    ) -> Image:
-        return add_background_mask(
             image, index, detections, opacity=self.opacity, color=self.color
         )

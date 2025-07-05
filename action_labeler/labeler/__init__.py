e06bc27ba362c0f0b98df4f44f0d5a030b6278a9
Justin Chang <justinchangmusic@gmail.com>
2025-07-05 11:32:24 -0400

    Update init with new folder structure

--- action_labeler/labeler/__init__.py
+++ action_labeler/labeler/__init__.py
@@ -1,4 +1,4 @@
-from .detection_labelers.dataset import LabelerDataset
-from .detection_labelers.labeler import ActionLabeler
+from .detection.dataset import LabelerDataset
+from .detection.labeler import ActionLabeler
 
 __all__ = ["ActionLabeler", "LabelerDataset"]
